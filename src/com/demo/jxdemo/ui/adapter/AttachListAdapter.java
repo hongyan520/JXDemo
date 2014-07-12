397572229da1757ccb8da58a95d09284da89265f
hongyan520 <354559242@qq.com>
2014-07-13 03:30:22 +0800

    update by yin.liu attach

--- src/com/demo/jxdemo/ui/adapter/AttachListAdapter.java
+++ src/com/demo/jxdemo/ui/adapter/AttachListAdapter.java
@@ -1,7 +1,20 @@
 package com.demo.jxdemo.ui.adapter;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.util.List;
 import java.util.Map;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 import android.content.Context;
 import android.content.Intent;
@@ -9,12 +22,14 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.demo.base.global.ActivityTaskManager;
@@ -31,6 +46,8 @@ import com.demo.jxdemo.ui.activity.menu.UserInfoActivity;
 public class AttachListAdapter extends BaseAdapter
 {
 
+	private static String tag = "AttachListAdapter";;
+
 	/**
 	 * Layout填充器
 	 */
@@ -46,6 +63,11 @@ public class AttachListAdapter extends BaseAdapter
 	 */
 	@SuppressWarnings("unused")
 	private Context context;
+	
+	private final static String DOWN_ON = "下载";
+	private final static String DOWN_ING = "下载中";
+	private final static String DOWN_WAIT = "暂停";
+	private final static String DOWN_OK_SEE = "查看";
 
 	/**
 	 * 构造方法
@@ -113,30 +135,39 @@ public class AttachListAdapter extends BaseAdapter
 		// 设置tag
 		convertView.setTag(holder);
 		
+		final String serverUrl = CommandConstants.URL_ROOT + StringUtil.Object2String(map.get("URL"));
+		final String localUrl = CacheSupport.staticServerUrlConvertToCachePath(serverUrl);
+		if(FileUtils.isExisitFile(localUrl)){
+			// 存在，已经下载
+			holder.rightBtn.setText(DOWN_OK_SEE);
+		}
+		
 		holder.rightBtn.setOnClickListener(new OnClickListener()
 		{
 
 			@Override
 			public void onClick(final View v)
 			{
-				final String serverUrl = CommandConstants.URL_ROOT + StringUtil.Object2String(map.get("URL"));
-				final String localUrl = CacheSupport.staticServerUrlConvertToCachePath(serverUrl);
-				new Thread()
-				{
-					public void run()
+				if(((Button)v).getText().equals(DOWN_ON)){// 点击下载开始下载
+					holder.progressBar.setVisibility(View.VISIBLE);
+					holder.rightBtn.setText(DOWN_ING);
+					new Thread()
 					{
-						if (HttpUtils.downloadFile(serverUrl, localUrl))
+						public void run()
 						{
-							Message msg = new Message();
-							msg.what = 1;
-							msg.obj = holder;
-							Bundle bundle = new Bundle();
-							bundle.putString("localUrl", localUrl);
-							msg.setData(bundle);
-							mHandler.sendMessage(msg);
-						}
-					};
-				}.start();
+							if (downloadFile(serverUrl, localUrl,mHandler,holder))
+							{
+								Message msg = new Message();
+								msg.what = 1;
+								msg.obj = holder;
+								Bundle bundle = new Bundle();
+								bundle.putString("localUrl", localUrl);
+								msg.setData(bundle);
+								mHandler.sendMessage(msg);
+							}
+						};
+					}.start();
+				}
 			}
 		});
 
@@ -154,14 +185,33 @@ public class AttachListAdapter extends BaseAdapter
 			{
 				case 1:
 					AttachListHolder holder = (AttachListHolder) msg.obj;
-					if ("下载".equals(holder.rightBtn.getText().toString()))
+					if (DOWN_ON.equals(holder.rightBtn.getText().toString()))
 					{
-						holder.rightBtn.setText("暂停");
+						holder.rightBtn.setText(DOWN_ING);
 					}
-					else if ("暂停".equals(holder.rightBtn.getText().toString()))
+					else if (DOWN_WAIT.equals(holder.rightBtn.getText().toString()))
 					{
-						holder.rightBtn.setText("下载");
+						holder.rightBtn.setText(DOWN_ON);
 					}
+					break;
+				case 2014:
+					// 下载中更新UI进度条
+					AttachListHolder aholder = (AttachListHolder) msg.obj;
+					int currentDownSize = msg.arg1;
+					if(currentDownSize == 100){ // 下载完成
+						try {
+							Thread.sleep(1000);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						aholder.rightBtn.setText(DOWN_OK_SEE);
+						aholder.progressBar.setVisibility(View.GONE);
+						aholder.progressBar.setProgress(0); // 复位进度条
+					}else{
+						aholder.progressBar.setProgress(currentDownSize);
+					}
+					
 					break;
 				default:
 					break;
@@ -179,6 +229,8 @@ public class AttachListAdapter extends BaseAdapter
 		Button leftIcon;
 
 		Button rightBtn;
+		
+		ProgressBar progressBar;
 
 		private AttachListHolder(View convertView)
 		{
@@ -186,6 +238,7 @@ public class AttachListAdapter extends BaseAdapter
 			textSize = (TextView) convertView.findViewById(R.id.text_size);
 			leftIcon = (Button) convertView.findViewById(R.id.btn_left);
 			rightBtn = (Button) convertView.findViewById(R.id.btn_right);
+			progressBar = (ProgressBar) convertView.findViewById(R.id.progressBar1);
 		}
 
 		private void setData(Map<String, Object> map)
@@ -200,9 +253,115 @@ public class AttachListAdapter extends BaseAdapter
 			}
 			b = url.replace(".", "!").split("!");
 			textTitle.setText(StringUtil.Object2String(url));
-			textSize.setText(StringUtil.Object2String(map.get("Size")));
+			textSize.setText(FileUtils.FormetFileSize(Long.parseLong(StringUtil.Object2String(map.get("Size")))));
 			leftIcon.setText(StringUtil.Object2String(b[b.length - 1]));
 		}
+	}
+	
+	
+	/**
+	 * 下载服务器文件
+	 * @param serverUrl 服务器地址
+	 * @param localUrl 本地存储地址
+	 * @return
+	 */
+	public static boolean downloadFile(String serverUrl, String localUrl,Handler mHandler,AttachListHolder mholder) {
+		InputStream input = null;
+		OutputStream output = null;
+		HttpURLConnection conn = null;
+		try {
+
+			File file = new File(localUrl);
+			if (file.exists()) {
+				Log.i(tag = "AttachListAdapter", localUrl + "is exist");
+				return true;
+			} else {
+				
+				//httpGet连接对象 
+		        HttpGet httpRequest = new HttpGet(serverUrl);
+		      //取得HttpClient 对象  
+		        HttpClient httpclient = new DefaultHttpClient();  
+		      //请求httpClient ，取得HttpRestponse  
+	            HttpResponse httpResponse = httpclient.execute(httpRequest);  
+	            if(httpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK){  
+	                //取得相关信息 取得HttpEntiy  
+	                HttpEntity httpEntity = httpResponse.getEntity();  
+	                //获得一个输入流  
+	                input = httpEntity.getContent();  
+	                int fileSize = 0;
+	                if(input != null){
+	                	fileSize = input.toString().length();
+	                }
+	                
+	                String dir = localUrl.substring(0,
+							localUrl.lastIndexOf("/") + 1);
+					new File(dir).mkdirs();// 新建文件夹
+					file.createNewFile();// 新建文件
+					output = new FileOutputStream(file);
+					byte[] buffer = new byte[1024];
+	                int len = 0;
+	                int lentemp = 0;
+	                while ((len = input.read(buffer)) != -1) {
+	                	output.write(buffer, 0, len);
+	                	if(mHandler != null){
+	                		Message msg = new Message();
+	                		msg.what = 2014;
+	                		msg.obj = mholder;
+	                		lentemp = lentemp + len;
+	                		msg.arg1 = (lentemp * 100) / fileSize;
+	                		mHandler.sendMessage(msg);
+	                		
+	                	}
+	                }
+	                if(mHandler != null){
+                		Message msg = new Message();
+                		msg.what = 2014;
+                		msg.obj = mholder;
+                		msg.arg1 = 100;
+                		mHandler.sendMessage(msg);
+                		
+                	}
+	                
+					
+					// 读取大文件
+//					byte[] buffer = new byte[4 * 1024];
+//					while (input.read(buffer) != -1) {
+//						output.write(buffer);
+//					}
+					output.flush();
+	            }
+				
+//				URL url = new URL(serverUrl);
+//				conn = (HttpURLConnection) url
+//						.openConnection();
+//				input = conn.getInputStream();
+				
+			}
+			Log.i(tag, localUrl + "download success");
+		} catch (Exception e) {
+			e.printStackTrace();
+			Log.i(tag, localUrl + "download fail");
+			return false;
+		} finally {
+			if(input != null){
+				try {
+					input.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			try {
+				if(output != null)
+					output.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			if(conn != null){
+				conn.disconnect();
+			}
+		}
+		return true;
 	}
 
 }
